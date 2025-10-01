e75e383e3f97ebaa7d222f1c32f77a4a1561c149
Shahana7000 <shahanacs2003@gmail.com>
2025-10-01 12:36:49 +0530

    frontend

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://backend.onrender.com",
+  baseURL: "//login-signup-forgetpass-4.onrender.com",
 });
 
 // attach token automatically
